609dad35ed5de261104a2823bb69974d6ea32a57
alyssonmill <alyssonmillerdev@gmail.com>
2024-06-19 19:18:34 +0100

    Created the shoping card. I just have to style it

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ let div = document.getElementById("cart_icon");
 let market = document.getElementById("market");
 
 div.addEventListener("click", () =>{
-    if(div){
+
+
+    if(market.style.display != "block"){
+        market.style.display = "block";
+    }else{
         market.style.display = "none";
     }
 })
+
+
+
 
 
 
